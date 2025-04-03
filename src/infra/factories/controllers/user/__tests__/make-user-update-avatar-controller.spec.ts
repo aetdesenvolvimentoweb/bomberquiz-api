8910ef12306b16a4cbca08ff718dc89e36ca8589
AET Desenvolvimento <aetdesenvolvimentoweb@gmail.com>
2025-04-03 17:31:47 -0300

    test: ensure that makeUserUpdateAvatarController correctly work

---
+++ src/infra/factories/controllers/user/__tests__/make-user-update-avatar-controller.spec.ts
@@ -0,0 +1,16 @@
+import { makeUserListController } from "@/infra/factories";
+import { UserListController } from "@/presentation/controllers";
+
+describe("makeUserListController", () => {
+  it("should create and return a UserListController instance with correct dependencies", () => {
+    // Arrange - Preparar tudo o que é necessário para o teste
+    // Neste caso, não precisamos de preparação especial
+
+    // Act - Executar a ação que queremos testar
+    const controller = makeUserListController();
+
+    // Assert - Verificar se o resultado é o esperado
+    expect(controller).toBeInstanceOf(UserListController);
+    expect(controller).toHaveProperty("handle");
+  });
+});
